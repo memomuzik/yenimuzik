9306d5fb062a5198204d4bacc2020f6dc9a12038
memomuzik <88396304+memomuzik@users.noreply.github.com>
2024-11-24 19:52:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/memomuzik/musik",
+    "https://github.com/memomuzik/yenimuzik",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
